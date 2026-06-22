212fa6e7f862880abb02f291432175d754093fa6
JADAV SRIKANTH <jadavsrikanth573@gmail.com>
2026-06-22 11:54:11 +0530

    Time: 3 ms (99.56%) | Memory: 14.2 MB (78.19%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -8,18 +8,19 @@ public:
         vector<bool> dp(target+1,false);
         dp[0]=true;
         for(int num: nums)
-        {
+        {                           fesible solution
             for(int j=target;j>=num;j--)
             {
                 dp[j]=dp[j]||dp[j-num];
             }
         }
+        return dp[target]
         */
         bitset<10001> dp;
         dp[0]=1;
         for(int num: nums)
         {
-            dp|=(dp<<num);
+            dp|=(dp<<num);            //optimal solution
         }
         return dp[s/2];
     }
