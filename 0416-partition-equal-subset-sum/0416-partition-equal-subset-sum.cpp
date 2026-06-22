0c2c36a20a2876d0ce444d44170fc405131f03b3
JADAV SRIKANTH <jadavsrikanth573@gmail.com>
2026-06-22 11:54:08 +0530

    Time: 3 ms (99.56%), Space: 14.2 MB (78.19%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -2,9 +2,26 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int s=accumulate(nums.begin(),nums.end(),0);
-        if(s%2==0)
-         return true;
-        return false;
-        
+        if(s&1)
+         return false;
+        /*int target=s/2;
+        vector<bool> dp(target+1,false);
+        dp[0]=true;
+        for(int num: nums)
+        {                           fesible solution
+            for(int j=target;j>=num;j--)
+            {
+                dp[j]=dp[j]||dp[j-num];
+            }
+        }
+        return dp[target]
+        */
+        bitset<10001> dp;
+        dp[0]=1;
+        for(int num: nums)
+        {
+            dp|=(dp<<num);            //optimal solution
+        }
+        return dp[s/2];
     }
 };
