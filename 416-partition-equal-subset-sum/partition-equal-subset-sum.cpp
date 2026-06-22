bc3833c2f9850a0d2ffa2c5dfbcd1372de77842a
JADAV SRIKANTH <jadavsrikanth573@gmail.com>
2026-06-22 11:52:36 +0530

    Time: 4 ms (99.35%) | Memory: 14.3 MB (77.82%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int s=accumulate(nums.begin(),nums.end(),0);
-        if(s%2!=0)
+        if(s&1)
          return false;
-        int target=s/2;
+        /*int target=s/2;
         vector<bool> dp(target+1,false);
         dp[0]=true;
         for(int num: nums)
@@ -14,6 +14,13 @@ public:
                 dp[j]=dp[j]||dp[j-num];
             }
         }
-        return dp[target];
+        */
+        bitset<10001> dp;
+        dp[0]=1;
+        for(int num: nums)
+        {
+            dp|=(dp<<num);
+        }
+        return dp[s/2];
     }
 };
