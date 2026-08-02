072d3c77968615cc5d1b2c47cf47f7f4972ab8be
JADAV SRIKANTH <jadavsrikanth573@gmail.com>
2026-08-02 18:55:52 +0530

    Time: 1 ms (85.84%), Space: 13.3 MB (74.75%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -27,4 +27,22 @@ public:
         return sri;
    
     }
+    //  this solution is also correct
+    /*vector<string> sri;
+    void generate(string curr,int open,int close,int n)
+    {
+
+    if(curr.size()==2*n)
+    { 
+       sri.push_back(curr);
+       return;
+    }
+    if(open<n)
+      generate(curr+"(",open+1,close,n);
+    if(close<open)
+       generate(curr+")",open, close+1,n);
+    }
+    vector<string> generateParenthesis(int n) {
+        generate("",0,0,n);
+        return sri;*/
 };
